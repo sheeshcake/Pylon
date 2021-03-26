294679ce9d9120577051a9bcee81a8573f773292
wendale1231 <spooderm5@gmail.com>
2021-03-26 12:42:19 +0800

    Change pictures and UI

--- resources/views/landingcontent/values.blade.php
+++ resources/views/landingcontent/values.blade.php
@@ -13,22 +13,27 @@
                 <div class="swiper-slide">
                     <div class="box" data-aos="fade-up" data-aos-delay="200">
                         <img src="assets/img/values-3.png" class="img-fluid" alt="">
-                        <h3>Branding Strategy</h3>
+                        <center><h3>Branding Strategy</h3>
                         <p>Branding strategy is a long-term plan for the development of a successful brand in order to achieve your specific goals. A well-defined strategic plan is about connecting to your consumers’ needs, emotions and competitive environment. </p>
+                        </center>
                     </div>
                 </div>
                 <div class="swiper-slide">
                     <div class="box" data-aos="fade-up" data-aos-delay="200">
                         <img src="assets/img/values-2.png" class="img-fluid" alt="">
+                        <center>
                         <h3>Social Media Marketing</h3>
                         <p>Social Media Marketing (SMM) is a form of internet marketing that includes creating and sharing content on social media networks such as Facebook and Instagram in order to achieve your marketing and branding goals. It is all about meeting your target audience and customers where they are and as they socially interact with each other and your brand. </p>
+                        </center>
                     </div>
                 </div>
                 <div class="swiper-slide">
                     <div class="box" data-aos="fade-up" data-aos-delay="200">
                         <img src="assets/img/values-1.png" class="img-fluid" alt="">
+                        <center>
                         <h3>Website Development</h3>
                         <p>Attract internet users to your brand through a powerful, attractive, and user-friendly website and convert them from target market individuals to your partners in business growth.</p>
+                        </center>
                     </div>
                 </div>
                 <div class="swiper-slide">
